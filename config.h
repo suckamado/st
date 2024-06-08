3004b8bd6a31b49ecedf4ed7a39b5ca99561d479
kamado <165230204+suckamado@users.noreply.github.com>
2024-06-08 22:40:15 +0100

    update st

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.4;
+float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
